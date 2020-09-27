81f07c9179fa08b8e39d595bd184ba8a86b72ad6
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-09-27 09:28:54 +0600

    problem

--- nikhojshongbad/urls.py
+++ nikhojshongbad/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('main.urls')),
 ]
-
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
